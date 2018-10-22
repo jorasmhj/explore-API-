b869f52d602d3718e1b92437af832b8e523d2784
jorasmhj <jorasmhj@gmail.com>
2018-10-22 20:56:42 +0545

    fix

--- config/database.php
+++ config/database.php
@@ -4,35 +4,6 @@ $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 
 return [
 
-    // …
-
-    'connections' => [
-
-        // …
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
-        ],
-
-        // …
-
-    ],
-
-    // …
-
-];
-
-return [
-
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
