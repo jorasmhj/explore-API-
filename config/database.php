0364d1a0ff135e452580eacde92dab8bca00f091
jorasmhj <jorasmhj@gmail.com>
2018-10-22 20:49:44 +0545

    fixes

--- config/database.php
+++ config/database.php
@@ -1,10 +1,35 @@
 <?php
 
 $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"]?? null;
-$username = $url["user"]??null;
-$password = $url["pass"]??null;
-$database = substr($url["path"], 1)??null;
+
+return [
+
+    // …
+
+    'connections' => [
+
+        // …
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'require',
+        ],
+
+        // …
+
+    ],
+
+    // …
+
+];
 
 return [
 
